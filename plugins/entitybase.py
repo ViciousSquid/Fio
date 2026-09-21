945eba98b94f426120bf10b980bc5b8849571318
Vicious Squid <161540961+ViciousSquid@users.noreply.github.com>
2026-09-21 03:20:57 +0100

    Keep plugin Prop render mode in sync

--- plugins/entitybase.py
+++ plugins/entitybase.py
@@ -103,7 +103,7 @@ class Prop(Model):
         super().__init__(pos, properties)
         self.properties['type'] = 'prop'
         for key, value in {
-            'sprite_path': 'assets/sprites/pickup.png', 'sprite_size': [32.0, 32.0], 'mass': 1.0,
+            'render_mode': 'model', 'sprite_path': 'assets/sprites/pickup.png', 'sprite_size': [32.0, 32.0], 'mass': 1.0,
             'collision_size': [0.0, 0.0, 0.0], 'no_collision': True,
             'collision_shape': 'auto',
             'physics_enabled': False, 'gravity': True, 'friction': 0.55,
